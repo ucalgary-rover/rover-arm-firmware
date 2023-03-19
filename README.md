ef47ec39be227cec2a37b079a8265fdc4228dd58
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-03-18 22:13:57 -0600

    Add firmware outline and g-code initial docs

--- README.md
+++ README.md
@@ -15,3 +15,15 @@ Firmware for the SSRT Rover arm's main microcontroller
 2. In VSCode, install the Platform.io extension.
 3. Clone this repo, and open it in VSCode.
 4. Uuuuh more.
+
+## Supported G-Code Commands
+The controller supports the following g-code-like commands. Note that the definitions of these commands differ from standard g-code (e.g., the way incremental vs. absolute moves are handled).
+
+All measurements are in degrees of the joint.
+
+* Absolute Move: `G0 A30 B42`
+* Incremental Move: `G1 A30 B42`
+* Dear controller, are you working?: `G31`
+	* Responds with `Doing great!`
+* Emergency Stop: `M112`
+	* Jumps queue.
--- RoverArmFirmware/src/main.cpp
+++ RoverArmFirmware/src/main.cpp
@@ -1,9 +1,56 @@
 #include <Arduino.h>
 
+#define PIN_MOTOR1_EN 4 // FIXME change to the correct number
+#define PIN_LIMIT1 4 // FIXME
+#define PIN_SERVO1_SIG // FIXME
+// ... (for all pins)
+
+// create AccelStepper instances
+// ...
+
+// create MultiStepper instance
+// ...
+
+// create current monitoring instances
+// ...
+
+
 void setup() {
-  // put your setup code here, to run once:
+	// setup steppers
+	// ...
+
+	Serial.begin(115200);
+	Serial.println("Booting...");
+
+	// make buzzer beep
+	// ... (there's a function that generates a square wave or something I'm pretty sure; try to use that)
+
+	// perform test of status LEDs
+	do_status_led_test();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	// if there is a pending line of Serial data, handle it by calling parse_gcode_and_execute_it()
+	// ... (something like if Serial.available() maybe?)
+	// ... parse_gcode_and_execute_it()
 }
+
+void do_status_led_test() {
+	// pulse each general LED
+	// ...
+}
+
+void exec_homing_sequence() { // FIXME devise a way to specify which motor we want to home as arguments (e.g., boolean for each motor maybe?)
+	// zero all the motors by moving them until they hit they limit switches, one-by-one
+}
+
+void log_currents() {
+	// output a UART log message with info about the current current consumption of the stepper motors
+	// example output: "CURRENT A330 B98.6 C23 D0"
+
+}
+
+void parse_gcode_and_execute_it() { // FIXME add an argument (either string or cstring), which is the gcode line to run
+	// Receive g-code like "G0 A30", and execute it
+}
+
